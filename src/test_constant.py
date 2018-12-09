e0bc228092fd693d770085071646a41830ba23f8
hfsf <hanniel88@gmail.com>
2018-12-08 22:43:05 -0300

    Changed (again and again) tests to /src/ folder in order to work with TravisCI

--- src/test_constant.py
+++ src/test_constant.py
@@ -11,8 +11,106 @@ sys.path[0] = str(root_dir)+'/src/'
 
 import pytest
 
-from .core import variable
+from .core import constant
 from .core import template_units
 
 import copy
 
+
+@pytest.fixture
+def const():
+    """
+    Create a generic Constant object with generic name, description and no value defined
+    """
+
+    return constant.Constant("generic_const",template_units.kg_s,"A generic const")
+
+def test_const_properties(const):
+
+    assert const.name == "generic_const" and \
+           str(const.units) == "kg^1 s^-1" and \
+           const.description == "A generic const"
+
+def test_set_const_value(const):
+
+    const.setValue(10)
+
+    assert const.value == 10
+
+
+@pytest.mark.parametrize("name,latex_text,operation", [
+    ("a","a","+"),
+    ("b","b","-"),
+    ("c","c","*"),
+    ("d","d","/"),
+    ("e","e","^")
+])
+
+def test_operations(const, name, latex_text, operation):
+    const.name = name
+    const.latex_text = latex_text
+
+    other = copy.deepcopy(const)
+    other.name = 'other_'+const.name
+    other.latex_text = other.name
+
+    #print("\n->const:{} const type:{}\nother:{} other type:{}".format(const,type(const),other,type(other)))
+    #print("\nconst dict:{}\n\nother dict:{}".format(const.__dict__, other.__dict__))
+
+    # Create an EquationNode
+
+    if operation == "+":
+
+        result = const() + other()
+
+        result_name = const.name+operation+other.name
+
+        latex_result = const.name+"+"+other.name
+
+        op_unit_result = str(const.units+other.units)
+
+    elif operation == "-":
+
+        result = const() - other()
+
+        result_name = const.name+operation+other.name
+
+        latex_result = const.name+"-"+other.name
+
+        op_unit_result = str(const.units-other.units)
+
+    elif operation == "*":
+
+        result = const() * other()
+
+        result_name = const.name+operation+other.name
+
+        latex_result = const.name+"*"+other.name
+
+        op_unit_result = str(const.units*other.units)
+
+    elif operation == "/":
+
+        result = const() / other()
+
+        result_name = const.name+operation+other.name
+
+        latex_result = "\\frac{"+const.name+"}{"+other.name+"}"
+
+        op_unit_result = str(const.units/other.units)
+
+    elif operation == "^":
+
+        result = const() ** 2
+
+        result_name = const.name+"**2"
+
+        latex_result = const.name+"^2"
+
+        op_unit_result = str(const.units**2)        
+
+    #print("\n result dict:{}".format(result.__dict__))
+
+    assert result.name == result_name and \
+           str(result.unit_object) == op_unit_result and \
+           result.latex_text == latex_result
--- src/test_parameter.py
+++ src/test_parameter.py
@@ -11,7 +11,105 @@ sys.path[0] = str(root_dir)+'/src/'
 
 import pytest
 
-from .core import variable
+from .core import parameter
 from .core import template_units
 
 import copy
+
+@pytest.fixture
+def param():
+    """
+    Create a generic Parameter object with generic name, description and no value defined
+    """
+
+    return parameter.Parameter("generic_param",template_units.kg_s,"A generic param")
+
+def test_param_properties(param):
+
+    assert param.name == "generic_param" and \
+           str(param.units) == "kg^1 s^-1" and \
+           param.description == "A generic param"
+
+def test_set_param_value(param):
+
+    param.setValue(10)
+
+    assert param.value == 10
+
+
+@pytest.mark.parametrize("name,latex_text,operation", [
+    ("a","a","+"),
+    ("b","b","-"),
+    ("c","c","*"),
+    ("d","d","/"),
+    ("e","e","^")
+])
+
+def test_operations(param, name, latex_text, operation):
+    param.name = name
+    param.latex_text = latex_text
+
+    other = copy.deepcopy(param)
+    other.name = 'other_'+param.name
+    other.latex_text = other.name
+
+    #print("\n->param:{} param type:{}\nother:{} other type:{}".format(param,type(param),other,type(other)))
+    #print("\nparam dict:{}\n\nother dict:{}".format(param.__dict__, other.__dict__))
+
+    # Create an EquationNode
+
+    if operation == "+":
+
+        result = param() + other()
+
+        result_name = param.name+operation+other.name
+
+        latex_result = param.name+"+"+other.name
+
+        op_unit_result = str(param.units+other.units)
+
+    elif operation == "-":
+
+        result = param() - other()
+
+        result_name = param.name+operation+other.name
+
+        latex_result = param.name+"-"+other.name
+
+        op_unit_result = str(param.units-other.units)
+
+    elif operation == "*":
+
+        result = param() * other()
+
+        result_name = param.name+operation+other.name
+
+        latex_result = param.name+"*"+other.name
+
+        op_unit_result = str(param.units*other.units)
+
+    elif operation == "/":
+
+        result = param() / other()
+
+        result_name = param.name+operation+other.name
+
+        latex_result = "\\frac{"+param.name+"}{"+other.name+"}"
+
+        op_unit_result = str(param.units/other.units)
+
+    elif operation == "^":
+
+        result = param() ** 2
+
+        result_name = param.name+"**2"
+
+        latex_result = param.name+"^2"
+
+        op_unit_result = str(param.units**2)        
+
+    #print("\n result dict:{}".format(result.__dict__))
+
+    assert result.name == result_name and \
+           str(result.unit_object) == op_unit_result and \
+           result.latex_text == latex_result
