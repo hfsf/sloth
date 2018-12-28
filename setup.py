74c4444dc3e3c1cb9367cea31495d1b3d3e65937
hfsf <hanniel88@gmail.com>
2018-12-28 13:46:35 -0300

    Include ipython3 in .travis.yml file

--- setup.py
+++ setup.py
@@ -102,7 +102,8 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    #packages=find_packages(exclude=('tests',)),    
+    packages=find_packages(),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
 
